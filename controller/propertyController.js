02405c67899a011295d7c6d58a8f4f8a7e8715ba
dewhizz <lewiskimari44@gmail.com>
2025-07-25 21:36:21 +0300

    updated

--- controller/propertyController.js
+++ controller/propertyController.js
@@ -1,14 +1,39 @@
-const {Property}=require('../model/SpaceDB')
+const {Property,User}=require('../model/SpaceDB')
+
+const multer =require('multer')
+const fs=require('fs')
+const path=require('path')
+
+// file location folder/directory
+const upload=multer({dest:'uploads/'})
 
 // add properties
 exports.uploadPropertyPhoto=upload.single('photo')
 exports.addProperty=async(req,res)=>{
     try {
-         const newProperty=req.body
-         console.log('inc',newProperty)
+         const {plotNumber,title,description} =req.body
+         
+       // prepare the upload file
+       let photo=null
+       if(req.file){
+            const ext=path.extname(req.file.originalname)
+            const newFileName=Date.now()+ext
+            const newPath = path.join('uploads',newFileName)
+            fs.renameSync(req.file.path,newPath)
+            photo=newPath.replace(/\\/g,'/')
+       }
+        
         // create a new property
-        const savedProperty=new Property(newProperty)
+        const savedProperty=new Property({
+             plotNumber,
+            title,
+            photo,
+            description
+        })
         console.log('inc',savedProperty)
+
+        const owner=req.user.userId
+         const newProperty=req.body
         await savedProperty.save()
         res.status(201).json(savedProperty)
     } catch (error) {
