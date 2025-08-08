f1e741310346026b4157a7e9a33d384a8a56c085
dewhizz <lewiskimari44@gmail.com>
2025-08-08 12:23:13 +0300

    Fri

--- routes/inquiryRoute.js
+++ routes/inquiryRoute.js
@@ -9,13 +9,13 @@ const { auth, authorizeRoles } = require("../middleware/auth");
 router.post("/",auth,inquiryController.addInquiry);
 
 // // user inquires
-// router.get("/",inquiryController.getUserInquiries);
+router.get("/myinquires",auth,inquiryController.getUserInquiries);
 
 // // owner inquires
 // router.get("/",inquiryController.getOwnerInquiries);
 
-router.get("/",inquiryController.getAllInquiries);
-router.get("/:id",auth,inquiryController.getOneById);
+// router.get("/",inquiryController.getAllInquiries);
+// router.get("/:id",auth,inquiryController.getOneById);
 router.put("/:id",auth,inquiryController.updateInquiry);
 router.delete("/:id", auth, inquiryController.deleteInquiry);
 
