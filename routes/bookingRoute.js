9a52cc2d9ea168fac520e69c29b542c9ceb7d965
dewhizz <lewiskimari44@gmail.com>
2025-08-09 23:45:30 +0300

    sat

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -6,7 +6,7 @@ const bookingController = require("../controller/bookingController")
 // authorization
 const { auth, authorizeRoles } = require("../middleware/auth");
 
-router.post("/:id",auth,bookingController.addBooking)
+router.post("/",auth,bookingController.addBooking)
 // user
 router.get("/myBookings",auth,bookingController.getMyBookings);
 
